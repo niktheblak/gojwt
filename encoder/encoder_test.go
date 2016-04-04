19a6aafd645cbe17ba243efdc24f91912e9981e2
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2016-04-04 12:20:10 +0300

    Moved context tests to encoder_test.go

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -1,13 +1,17 @@
-package jwt
+package encoder
 
 import (
 	"testing"
 
-	"github.com/niktheblak/jwt/encoder"
 	"github.com/stretchr/testify/assert"
 )
 
 var testSecret = []byte("secret")
+
+var testHeader = map[string]interface{}{
+	"alg": "HS256",
+	"typ": "JWT",
+}
 
 var testClaims = map[string]interface{}{
 	"sub":   "1234567890",
@@ -18,27 +22,28 @@ var testClaims = map[string]interface{}{
 var testToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMTIzNDU2Nzg5MCJ9.eNK_fimsCW3Q-meOXyc_dnZHubl2D4eZkIcn6llniCk"
 
 func TestEncode(t *testing.T) {
-	ctx := NewContext(testSecret)
-	token := New()
-	token.Claims = testClaims
-	encoded, err := ctx.Encode(token)
-	assert.Equal(t, testToken, string(encoded))
+	token := Token{
+		Header: testHeader,
+		Claims: testClaims,
+	}
+	encoded, err := Encode(testSecret, token)
 	assert.NoError(t, err)
+	assert.Equal(t, testToken, encoded)
 }
 
 func TestDecode(t *testing.T) {
-	ctx := NewContext(testSecret)
-	token, err := ctx.Decode(testToken)
+	token, err := Decode(testSecret, testToken)
 	assert.NoError(t, err)
 	assert.Equal(t, testClaims, token.Claims)
 }
 
 func TestSignature(t *testing.T) {
-	ctx := NewContext(testSecret)
-	token := New()
-	token.Claims = testClaims
-	encoded, err := ctx.Encode(token)
+	token := Token{
+		Header: testHeader,
+		Claims: testClaims,
+	}
+	encoded, err := Encode(testSecret, token)
 	assert.NoError(t, err)
-	err = encoder.VerifySignature(testSecret, encoded)
+	err = VerifySignature(testSecret, encoded)
 	assert.NoError(t, err)
 }
