8fb0dee9d28004ccb6cd9a18295f62895d9834ee
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-08-15 12:52:00 +0300

    refactor: stricter token algorithm validation

--- pkg/signing/token.go
+++ pkg/signing/token.go
@@ -8,41 +8,12 @@ import (
 )
 
 var (
-	ErrInvalidAlgorithm = errors.New("invalid signing algorithm")
+	ErrUnexpectedAlgorithm = errors.New("unexpected signing algorithm")
 )
 
 func ValidateMethod(algorithm string, token *jwt.Token) error {
-	family, err := GetFamilyFromSigningMethod(token.Method)
-	if err != nil {
-		return err
-	}
-	tokenFamily, err := GetFamily(algorithm)
-	if err != nil {
-		return err
-	}
-	switch family {
-	case HMAC:
-		if tokenFamily != HMAC {
-			return ErrInvalidAlgorithm
-		}
-	case RSA:
-		if tokenFamily != RSA {
-			return ErrInvalidAlgorithm
-		}
-	case ECDSA:
-		if tokenFamily != ECDSA {
-			return ErrInvalidAlgorithm
-		}
-	case EdDSA:
-		if tokenFamily != EdDSA {
-			return ErrInvalidAlgorithm
-		}
-	case RSAPSS:
-		if tokenFamily != RSAPSS {
-			return ErrInvalidAlgorithm
-		}
-	default:
-		return fmt.Errorf("%w: %v", ErrUnknownAlgorithm, token.Header["alg"])
+	if algorithm != token.Method.Alg() {
+		return fmt.Errorf("%w: %v", ErrUnexpectedAlgorithm, token.Header["alg"])
 	}
 	return nil
 }
