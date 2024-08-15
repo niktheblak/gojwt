ed85ff6a21a9393405fe19c68f8f64907e9e6102
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-08-15 13:47:59 +0300

    feat: decode now takes input files as positional arguments
    BREAKING CHANGE: the -i argument is no longer used

--- cmd/decode.go
+++ cmd/decode.go
@@ -15,7 +15,6 @@ import (
 )
 
 var (
-	input string
 	force bool
 )
 
@@ -23,6 +22,7 @@ var decodeCmd = &cobra.Command{
 	Use:          "decode",
 	Short:        "Decodes and prints the contents of the given JWT token",
 	SilenceUsage: true,
+	Args:         cobra.MinimumNArgs(0),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if publicKeyPath == "" {
 			return fmt.Errorf("argument --public-key must be specified")
@@ -30,45 +30,52 @@ var decodeCmd = &cobra.Command{
 		if err := signing.ValidateAlgorithm(algorithm); err != nil {
 			return err
 		}
-		var tokenStr string
-		if input != "" {
-			content, err := os.ReadFile(input)
-			if err != nil {
-				return err
+		if len(args) > 0 {
+			for _, path := range args {
+				content, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
+				tokenStr := string(content)
+				if err := parseToken(cmd, tokenStr); err != nil {
+					return err
+				}
 			}
-			tokenStr = string(content)
-		} else {
-			scanner := bufio.NewScanner(os.Stdin)
-			scanner.Scan()
-			if scanner.Err() != nil {
-				return scanner.Err()
-			}
-			tokenStr = strings.TrimSpace(scanner.Text())
+			return nil
 		}
-		var invalidToken jwt.Token
-		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-			invalidToken = *token
-			if err := signing.ValidateMethod(algorithm, token); err != nil {
-				return nil, err
-			}
-			return signing.LoadVerifyKey(algorithm, publicKeyPath)
-		})
-		if err != nil {
-			if force {
-				fmt.Fprintf(cmd.ErrOrStderr(), "Warning: token is not valid: %v\n", err)
-				return printToken(cmd.OutOrStdout(), &invalidToken)
-			}
-			return err
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		if scanner.Err() != nil {
+			return scanner.Err()
 		}
-		return printToken(cmd.OutOrStdout(), token)
+		tokenStr := strings.TrimSpace(scanner.Text())
+		return parseToken(cmd, tokenStr)
 	},
 }
 
 func init() {
-	decodeCmd.Flags().StringVarP(&input, "input", "i", "", "read JWT token from file (stdin if empty)")
 	decodeCmd.Flags().BoolVarP(&force, "force", "f", false, "print token data even if token is not valid")
 
 	rootCmd.AddCommand(decodeCmd)
+}
+
+func parseToken(cmd *cobra.Command, tokenStr string) error {
+	var invalidToken jwt.Token
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		invalidToken = *token
+		if err := signing.ValidateMethod(algorithm, token); err != nil {
+			return nil, err
+		}
+		return signing.LoadVerifyKey(algorithm, publicKeyPath)
+	})
+	if err != nil {
+		if force {
+			fmt.Fprintf(cmd.ErrOrStderr(), "Warning: token is not valid: %v\n", err)
+			return printToken(cmd.OutOrStdout(), &invalidToken)
+		}
+		return err
+	}
+	return printToken(cmd.OutOrStdout(), token)
 }
 
 func printToken(w io.Writer, token *jwt.Token) error {
